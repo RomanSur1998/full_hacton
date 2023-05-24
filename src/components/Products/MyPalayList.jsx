a7e88cbf3c2f3c1505ca2898ff39167fea09d45d
RomanSur1998 <123935997+RomanSur1998@users.noreply.github.com>
2023-05-24 16:56:10 +0600

    save block main

--- src/components/Products/MyPalayList.jsx
+++ src/components/Products/MyPalayList.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-import heart from "../../assets/Vector.png";
+import heart from "../../assets/Vector.svg";
 import classes from "..//style/MuPlayList.module.css";
 import play from "../../assets/Play.svg";
 import download from "../../assets/Line=empty, Name=download.svg";
 import search from "../../assets/Line=bold, Name=search.svg";
 import drop from "../../assets/fi-ss-caret-down.svg";
 import clock from "../../assets/Line=Clock.svg";
+import song from "../../assets/Rectangle 236.svg";
 
 const MyPalayList = () => {
   return (
@@ -27,94 +28,45 @@ const MyPalayList = () => {
             <img src={download} alt="" />
           </div>
           <div className={classes.track_props_right}>
-            <img src={search} alt="" />
+            <img src={search} alt="" style={{ width: "25px" }} />
             <span>Дата добавления </span>
             <img src={drop} alt="" />
           </div>
         </div>
         <div className={classes.track_line_head}>
-          <table>
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>Название</th>
-                <th>Альбом</th>
-                <th>Дата добавления</th>
-                <th>
-                  <img src={clock} alt="" />
-                </th>
-              </tr>
-            </thead>
-            {/* <hr /> */}
-            <tbody>
-              <tr className={classes.track_line}>
-                <td>
-                  <img src={play} alt="" />
-                </td>
-                <td>
-                  <img src="" alt="" />
-                  <h5> Название трека </h5>
-                  <h5> Имя исполнителя </h5>
-                </td>
-                <td>Называние альбома</td>
-                <td>Дата добавления</td>
-                <td>Длинна трека</td>
-              </tr>
-
-              <tr className={classes.track_line}>
-                <td>
-                  <img src={play} alt="" />
-                </td>
-                <td>
-                  <img src="" alt="" />
-                  <h5> Название трека </h5>
-                  <h5> Имя исполнителя </h5>
-                </td>
-                <td>Называние альбома</td>
-                <td>Дата добавления</td>
-                <td>Длинна трека</td>
-              </tr>
-              <tr className={classes.track_line}>
-                <td>
-                  <img src={play} alt="" />
-                </td>
-                <td>
-                  <img src="" alt="" />
-                  <h5> Название трека </h5>
-                  <h5> Имя исполнителя </h5>
-                </td>
-                <td>Называние альбома</td>
-                <td>Дата добавления</td>
-                <td>Длинна трека</td>
-              </tr>
-              <tr className={classes.track_line}>
-                <td>
-                  <img src={play} alt="" />
-                </td>
-                <td>
-                  <img src="" alt="" />
-                  <h5> Название трека </h5>
-                  <h5> Имя исполнителя </h5>
-                </td>
-                <td>Называние альбома</td>
-                <td>Дата добавления</td>
-                <td>Длинна трека</td>
-              </tr>
-              <tr className={classes.track_line}>
-                <td>
-                  <img src={play} alt="" />
-                </td>
-                <td>
-                  <img src="" alt="" />
-                  <h5> Название трека </h5>
-                  <h5> Имя исполнителя </h5>
-                </td>
-                <td>Называние альбома</td>
-                <td>Дата добавления</td>
-                <td>Длинна трека</td>
-              </tr>
-            </tbody>
-          </table>
+          <div className={classes.container_grid}>
+            <div>
+              <h4>#</h4>
+            </div>
+            <div>
+              <h4>Name</h4>
+            </div>
+            <div>
+              <h4>Album</h4>
+            </div>
+            <div>
+              <h4>Data puplic</h4>
+            </div>
+            <div>
+              <img src={clock} alt="" />
+            </div>
+          </div>
+          <div className={classes.track_line}>
+            <div>
+              {" "}
+              <img src={play} alt="" />
+            </div>
+            <div className={classes.track_line_section}>
+              <img src={song} alt="" />
+              <div className={classes.track_line_section_name}>
+                <h4> Kill Bill </h4>
+                <h5> SZA </h5>
+              </div>
+            </div>
+            <div>SOS</div>
+            <div>1 day ago</div>
+            <div>3:22</div>
+          </div>
         </div>
       </div>
     </div>
