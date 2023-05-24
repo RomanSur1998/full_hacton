993f5ccae15bc25d0e4a91757056c95c04bf4aca
Адилет Газыбеков <adiletgazybekov@MacBook-Air-Adilet.local>
2023-05-24 16:53:05 +0600

    save

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,5 @@
 import React from "react";
 import "./Main.css";
-import Facebook from "../assets/facebook.svg";
-import Twitter from "../assets/twitter.svg";
-import Insta from "../assets/instagram.svg";
-import Pinterest from "../assets/pinterest.svg";
-import Youtube from "../assets/youtube.svg";
 
 const Main = () => {
   return (
@@ -13,26 +8,182 @@ const Main = () => {
         <h3>Try something else</h3>
         <div className="cards">
           <div className="card">
-            <img
-              style={{
-                width: "90%",
-                height: "60%",
-                marginLeft: "10px",
-                borderRadius: "7px",
-              }}
-              src="https://static01.nyt.com/images/2022/06/28/arts/28CONAN-GRAY1/28CONAN-GRAY1-superJumbo.jpg"
-              alt="картинка"
-            />
-            <span>teen beats</span>
-            <p>Conan Gray has entered the upside down</p>
+            <div className="img-holder">
+              <img
+                src="https://static01.nyt.com/images/2022/06/28/arts/28CONAN-GRAY1/28CONAN-GRAY1-superJumbo.jpg"
+                alt="image"
+              />
+            </div>
+            <div className="text">
+              <h2>teen beats</h2>
+              <p>Conan Gray has entered the upside down</p>
+            </div>
+            <div className="play-icon">
+              <div className="circle">
+                <div className="triangle"></div>
+              </div>
+            </div>
+          </div>
+          <div className="card">
+            <div className="img-holder">
+              <img
+                src="https://static01.nyt.com/images/2022/06/28/arts/28CONAN-GRAY1/28CONAN-GRAY1-superJumbo.jpg"
+                alt="image"
+              />
+            </div>
+            <div className="text">
+              <h2>teen beats</h2>
+              <p>Conan Gray has entered the upside down</p>
+            </div>
+            <div className="play-icon">
+              <div className="circle">
+                <div className="triangle"></div>
+              </div>
+            </div>
+          </div>
+          <div className="card">
+            <div className="img-holder">
+              <img
+                src="https://static01.nyt.com/images/2022/06/28/arts/28CONAN-GRAY1/28CONAN-GRAY1-superJumbo.jpg"
+                alt="image"
+              />
+            </div>
+            <div className="text">
+              <h2>teen beats</h2>
+              <p>Conan Gray has entered the upside down</p>
+            </div>
+            <div className="play-icon">
+              <div className="circle">
+                <div className="triangle"></div>
+              </div>
+            </div>
+          </div>
+          <div className="card">
+            <div className="img-holder">
+              <img
+                src="https://static01.nyt.com/images/2022/06/28/arts/28CONAN-GRAY1/28CONAN-GRAY1-superJumbo.jpg"
+                alt="image"
+              />
+            </div>
+            <div className="text">
+              <h2>teen beats</h2>
+              <p>Conan Gray has entered the upside down</p>
+            </div>
+            <div className="play-icon">
+              <div className="circle">
+                <div className="triangle"></div>
+              </div>
+            </div>
+          </div>
+          <div className="card">
+            <div className="img-holder">
+              <img
+                src="https://static01.nyt.com/images/2022/06/28/arts/28CONAN-GRAY1/28CONAN-GRAY1-superJumbo.jpg"
+                alt="image"
+              />
+            </div>
+            <div className="text">
+              <h2>teen beats</h2>
+              <p>Conan Gray has entered the upside down</p>
+            </div>
+            <div className="play-icon">
+              <div className="circle">
+                <div className="triangle"></div>
+              </div>
+            </div>
+          </div>
+          <div className="card">
+            <div className="img-holder">
+              <img
+                src="https://static01.nyt.com/images/2022/06/28/arts/28CONAN-GRAY1/28CONAN-GRAY1-superJumbo.jpg"
+                alt="image"
+              />
+            </div>
+            <div className="text">
+              <h2>teen beats</h2>
+              <p>Conan Gray has entered the upside down</p>
+            </div>
+            <div className="play-icon">
+              <div className="circle">
+                <div className="triangle"></div>
+              </div>
+            </div>
+          </div>
+          <div className="card">
+            <div className="img-holder">
+              <img
+                src="https://static01.nyt.com/images/2022/06/28/arts/28CONAN-GRAY1/28CONAN-GRAY1-superJumbo.jpg"
+                alt="image"
+              />
+            </div>
+            <div className="text">
+              <h2>teen beats</h2>
+              <p>Conan Gray has entered the upside down</p>
+            </div>
+            <div className="play-icon">
+              <div className="circle">
+                <div className="triangle"></div>
+              </div>
+            </div>
+          </div>
+          <div className="card">
+            <div className="img-holder">
+              <img
+                src="https://static01.nyt.com/images/2022/06/28/arts/28CONAN-GRAY1/28CONAN-GRAY1-superJumbo.jpg"
+                alt="image"
+              />
+            </div>
+            <div className="text">
+              <h2>teen beats</h2>
+              <p>Conan Gray has entered the upside down</p>
+            </div>
+            <div className="play-icon">
+              <div className="circle">
+                <div className="triangle"></div>
+              </div>
+            </div>
+          </div>
+          <div className="card">
+            <div className="img-holder">
+              <img
+                src="https://static01.nyt.com/images/2022/06/28/arts/28CONAN-GRAY1/28CONAN-GRAY1-superJumbo.jpg"
+                alt="image"
+              />
+            </div>
+            <div className="text">
+              <h2>teen beats</h2>
+              <p>Conan Gray has entered the upside down</p>
+            </div>
+            <div className="play-icon">
+              <div className="circle">
+                <div className="triangle"></div>
+              </div>
+            </div>
+          </div>
+          <div className="card">
+            <div className="img-holder">
+              <img
+                src="https://static01.nyt.com/images/2022/06/28/arts/28CONAN-GRAY1/28CONAN-GRAY1-superJumbo.jpg"
+                alt="image"
+              />
+            </div>
+            <div className="text">
+              <h2>teen beats</h2>
+              <p>Conan Gray has entered the upside down</p>
+            </div>
+            <div className="play-icon">
+              <div className="circle">
+                <div className="triangle"></div>
+              </div>
+            </div>
           </div>
         </div>
         <footer>
           <div className="footer__container">
             <div className="footer__left">
               <div className="support1">
-                <span>Company</span>
-                <span>Communities</span>
+                <h3>Company</h3>
+                <h3>Communities</h3>
 
                 <div className="support">
                   <div className="support__left">
